a26abfc41359f9e1d6f63796b657ac529d5f33b2
sandor_orosz <SsamcroO#8989>
2019-04-09 17:11:50 +0200

    initial commit

---
+++ framework/protractor.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const GLOBAL_TIMEOUT = 40e3;
+const os = require('os');
+const path = require('path');
+const requireIt = require('require-it');
+
+exports.config = {
+    specs: 'specs/**/*.spec.js',
+    capabilities: {
+        browserName: 'chrome'
+    },
+    directConnect: true,
+    mochaOpts: {
+        reporter: 'spec'
+    },
+    framework: 'mocha',
+    getPageTimeout: GLOBAL_TIMEOUT,
+    chromeDriver: path.join(requireIt.directory('protractor'), 'node_modules', 'webdriver-manager', 'selenium', 'chromedriver_2.46' + (os.platform() === 'win32' ? '.exe' : '')),
+    onPrepare: function () {
+        global.GLOBAL_TIMEOUT = GLOBAL_TIMEOUT;
+        global.ec = protractor.ExpectedConditions;
+
+        const chai = require('chai');
+        chai.use(require('chai-as-promised'));
+        global.expect = chai.expect;
+
+        browser.waitForAngularEnabled(false);
+        return browser.manage().window().maximize().catch(() => {
+            return browser.manage().window().setSize(1800, 1012);
+        });
+    }
+};
