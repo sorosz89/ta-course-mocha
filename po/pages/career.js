d81a12376e40d98c34401539010e871befea5aef
Sándor Orosz <os.sandor@gmail.com>
2019-04-10 20:26:26 +0200

    mocha setup

---
+++ po/pages/career.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class CareerPage {
+    constructor() {
+        this.logo = element(by.css('img.header__logo'));
+        this.slider = element(by.css('.slider'))
+        this.searchForm = element(by.css('form.recruiting-search__form'));
+    }
+
+    async load() {
+        browser.get('https://epam.com/careers');
+        return browser.wait(() => this.isLogoDisplayed());
+    }
+
+    async isLogoDisplayed(){
+        return this.logo.isDisplayed();
+    }
+}
+
+module.exports = CareerPage;
